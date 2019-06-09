0ac88ab1fcf977ca1334d1316491ad972a5cbeb3
Kira Corbin <madkatkeys@gmail.com>
2019-06-09 09:14:42 -0700

    finished forms  added todoArray variable todoArray push and consolelog

--- assets/app.js
+++ assets/app.js
@@ -1,9 +1,13 @@
 // id="todoText"
 // button id="todoBtn"
 $(document).ready(function(){
+    let todoArray = []
+
     $("#todoBtn").on("click", function(){
         event.preventDefault()
         var text = $("#todoText").val()
+        todoArray.push("todoText")
+        console.log(todoArray)
         $("#todoGroup").append(
             `<li class="list-group-item">${text}</li>`
         )
