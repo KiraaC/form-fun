4a3ecc9b69dfafa6bc074857af9aa7875bacc57e
Kira Corbin <madkatkeys@gmail.com>
2019-06-09 09:33:11 -0700

    finished adding ajax queryUrl methods and function response with quotes

--- assets/app.js
+++ assets/app.js
@@ -1,19 +1,31 @@
-// id="todoText"
+// text id="todoText" 
 // button id="todoBtn"
-$(document).ready(function(){
+$(document).ready(function () {
     let todoArray = []
+    var queryUrl = "http://quotes.rest/qod.json?category=inspire"
 
-    $("#todoBtn").on("click", function(){
+
+    $("#todoBtn").on("click", function (event) {
         event.preventDefault()
         var text = $("#todoText").val()
-        todoArray.push("todoText")
+        todoArray.push(text)
         console.log(todoArray)
-        $("#todoGroup").empty()
-        for (let i=0; i < todoArray.length; i++){
+        // $("#todoGroup").empty()
+        // for (let i = 0; i < todoArray.length; i++) {
+        //     $("#todoGroup").append(
+        //         `<li class="list-group-item">${todoArray[i]}</li>`
+        //     )
+        // }
+        $.ajax({
+            url: queryUrl,
+            method: "GET"
+        }).then(function (response) {
+            console.log(response.contents.quotes[0].quote)
+            $("#quote-text").html(
+                `<p>${response.contents.quotes[0].quote}</p>`
+            )
 
-        }
-        $("#todoGroup").append(
-            `<li class="list-group-item">${todoArray[i]}</li>`
-        )
+        })
+
     })
 })
