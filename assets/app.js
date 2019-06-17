b8b195bfef08373863d260b942a71b5a5be5d2ff
Kira Corbin <madkatkeys@gmail.com>
2019-06-17 09:52:32 -0700

    first commit

--- assets/app.js
+++ assets/app.js
@@ -1,15 +1,15 @@
 // text id="todoText" 
 // button id="todoBtn"
-$(document).ready(function () {
+$(document).ready(function() {
     let todoArray = []
     var queryUrl = "http://quotes.rest/qod.json?category=inspire"
 
 
-    $("#todoBtn").on("click", function (event) {
+    $("#todoBtn").on("click", function(event) {
         event.preventDefault()
         var text = $("#todoText").val()
         todoArray.push(text)
-        console.log(todoArray)
+        // console.log(todoArray)
         // $("#todoGroup").empty()
         // for (let i = 0; i < todoArray.length; i++) {
         //     $("#todoGroup").append(
@@ -19,13 +19,11 @@ $(document).ready(function () {
         $.ajax({
             url: queryUrl,
             method: "GET"
-        }).then(function (response) {
+        }).then(function(response) {
             console.log(response.contents.quotes[0].quote)
             $("#quote-text").html(
                 `<p>${response.contents.quotes[0].quote}</p>`
             )
-
         })
-
     })
 })
